a75513cca3b3e73db0c8fff1f0b5cb14198fd305
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2021-05-21 23:48:55 +0200

    passing-vowel-test-surname-code

--- src/code-generator.js
+++ src/code-generator.js
@@ -7,9 +7,25 @@ class fiscalCode {
         this.birthCity = birthCity;
     }
     get surnameCode() {
-        const consonant = this.surname.match(/[bcdfghjklmnpqrstvwxys]/gi).slice(0,3).join("");
-        return consonant;
+        const consonant = this.surname.match(/[bcdfghjklmnpqrstvwxys]/gi);
+        const vowel = this.surname.match(/[aeiou]/gi);
+        if(consonant && consonant.length > 0 ) {
+            return surnameGenerator(consonant, vowel);
+        } else {
+            return vowel.slice(0,3).join("");
+        }
+       
     }
 }
+
+function surnameGenerator(consonant, vowel) {
+        const fiscalConsonant = consonant.slice(0,3).join("");
+        if(fiscalConsonant.length === 3) {
+            return fiscalConsonant;
+        } else {
+            const addVowel = fiscalConsonant + vowel.slice(0,3-fiscalConsonant.length).join("");
+            return addVowel;
+        }
+} 
 
 module.exports = fiscalCode;
--- test/code-generator.test.js
+++ test/code-generator.test.js
@@ -1,9 +1,19 @@
 const fiscalGenerator = require("../src/code-generator");
 
 describe('should generate the fiscal code', () => {
-    
+
     const codeGenerator = new fiscalGenerator("", "Bertoldi","","","");
     test("should return 3 consonant of the surname", () => {
         expect(codeGenerator.surnameCode).toBe("Brt");
+    });
+
+    const codeGenerator2 = new fiscalGenerator("", "Beroi","","","");
+    test("should return 2 consonant and 1 vowel of the surname", () => {
+        expect(codeGenerator2.surnameCode).toBe("Bre");
+    });
+
+    const codeGenerator3 = new fiscalGenerator("", "Aeooi","","","");
+    test("should return 3 vowel of the surname", () => {
+        expect(codeGenerator3.surnameCode).toBe("Aeo");
     });
 })
